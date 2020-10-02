221aa9a1786e70528c591cb1d759ec50356fda89
EncryptedWeb <48656828+EncryptedWeb@users.noreply.github.com>
2020-10-03 00:24:54 +0200

    Inital Comit
    Upload all files, Just have to publish release v1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# SAH pc prepare
- SAH Computer setup.
+# 💻SAH Computer setup💻
+ Dit programma is gemaakt voor Student Aan Huis. Dit programma is gemaakt om basics voor een pc te installeren.
+
+## What is this?
+ SAH = StudentAanHuis
+ StudentAanHuis is a computer issue solve company
---
+++ SAH-Prepare/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SAH_Prepare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom");
+            Console.WriteLine("Dit is de SAH Prepare tool!");
+            Console.WriteLine("Gemaakt door Justin Pooters");
+            Console.WriteLine("Typ `ja` om door te gaan of typ `nee` om te stoppen.");
+            string yesno = Console.ReadLine();
+            if (yesno == "nee")
+            {
+                Console.WriteLine("Druk op Enter om af te sluiten");
+                Console.ReadLine();
+            } 
+            else
+            {
+                Console.WriteLine("De installatie kan nu beginnen");
+                Console.WriteLine("Kies een van de volgende opties");
+                Console.WriteLine("1: Full install, dit installeert: ThunderBird, CCleaner, Malwarebytes, Avast anti-virus en FireFox web browser");
+                Console.WriteLine("2: AntiVirus, dit installeert Malwarebytes en Avast anti-virus");
+                Console.WriteLine("3: Standaard, Dit installeert Firefox en ThunderBird");
+                Console.WriteLine("Kis 1, 2 of 3");
+                string gekozennummer = Console.ReadLine();
+                if(gekozennummer == "1")
+                {
+                    //Avast
+                    
+                    Console.WriteLine("Cool! Full install");
+                    Console.WriteLine("Laten we starte met Anti-Virus!");
+                    Console.WriteLine("Meestal is dat wel het belangerijkste of niet? :P");
+                    
+                    //avast
+
+                    Console.WriteLine("Starting avast download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/antivirus.exe", "antivirus.exe");
+
+                    // MalwareBytes
+
+                    Console.WriteLine("Starting MalewareBytes download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/malwarebytes.exe", "malwarebytes.exe");
+
+                    Console.WriteLine("FireFox & Thunderbird are next! :D");
+                    Console.WriteLine("Wie wil er nog internet explorer / edge gebruiken 🤮");
+
+
+                    //Firefox
+
+                    Console.WriteLine("Starting firefox download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/firefox.exe", "firefox.exe");
+
+                    //Thunderbird
+
+                    Console.WriteLine("Starting thunderbird download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/thunderbird.exe", "thunderbird.exe");
+
+                    Console.WriteLine("Extra handy dandy tools ;P");
+
+
+                    // CCleaner
+
+                    Console.WriteLine("Starting ccleaner download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/ccleaner.exe", "ccleaner.exe");
+
+                }
+                if (gekozennummer == "2")
+                {
+                    Console.WriteLine("Cool! Antivirus, safety first ;)");
+                    
+                    // Avast
+
+                    Console.WriteLine("Starting avast download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/antivirus.exe", "antivirus.exe");
+
+                    // MalwareBytes
+
+                    Console.WriteLine("Starting malwarebytes download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/malwarebytes.exe", "malwarebytes.exe");
+
+                    // CCleaner
+
+                    Console.WriteLine("Starting ccleaner download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/ccleaner.exe", "ccleaner.exe");
+
+                }
+
+                if (gekozennummer == "3")
+                {
+                    Console.WriteLine("Cool! internet en email zijn belangerijk hoor :P");
+
+                    //Firefox
+
+                    Console.WriteLine("Starting Browser download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/firefox.exe", "firefox.exe");
+
+                    //Thunderbird
+                    Console.WriteLine("Starting Thunderbird download");
+                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/thunderbird.exe", "thunderbird.exe");
+
+                }
+            }
+        }
+        private static void DownloadAndExecute(string url, string outputFileName)
+        {
+            AutoResetEvent are = new AutoResetEvent(false);
+            var client = new WebClient();
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler((sender, args) =>
+                {
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), outputFileName);
+                    System.Diagnostics.Process.Start(path);
+                    are.Set();
+                }); 
+                
+            client.DownloadFileAsync(new Uri(url), outputFileName);
+
+            are.WaitOne();
+            client.Dispose();
+        }
+
+    }
+}
