353086c1f89dde3da35f6a4b6f17835ed7a74f6d
Aaron Knoop <aaron.knoop@live.nl>
2020-10-09 12:57:17 +0200

    Refactored code
    Removed a lot of repeating code ( AKA I applied the DRY Principle )
    I also made sure that the program can't crash when there is unexpected input ( Improvement of lifetime )

--- SAH-Prepare/Program.cs
+++ SAH-Prepare/Program.cs
@@ -11,152 +11,145 @@ using System.Threading.Tasks;
 
 namespace SAH_Prepare
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welkom");
-            Console.WriteLine("Dit is de SAH Prepare tool!");
-            Console.WriteLine("Gemaakt door Justin Pooters");
-            Console.WriteLine("Typ `ja` om door te gaan of typ `nee` om te stoppen.");
-            string yesno = Console.ReadLine();
-            if (yesno == "nee")
-            {
-                Console.WriteLine("Druk op Enter om af te sluiten");
-                Console.ReadLine();
-            } 
-            else
-            {
-                Console.WriteLine("De installatie kan nu beginnen");
-                Console.WriteLine("Kies een van de volgende opties");
-                Console.WriteLine("1: Full install, dit installeert: ThunderBird, CCleaner, Malwarebytes, Avast anti-virus en FireFox web browser");
-                Console.WriteLine("2: AntiVirus, dit installeert Malwarebytes en Avast anti-virus");
-                Console.WriteLine("3: Standaard, Dit installeert Firefox en ThunderBird");
-                Console.WriteLine("Kis 1, 2 of 3");
-                string gekozennummer = Console.ReadLine();
-                if(gekozennummer == "1")
-                {
-                    Console.WriteLine("Wil je avast ook installeren?");
-                    Console.WriteLine("Kies: ja of nee (*GEEN HOOFDLETTERS*)");
-                    if (Console.ReadLine() == "ja")
-                    {
-                        //avast
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welkom");
+			Console.WriteLine("Dit is de SAH Prepare tool!");
+			Console.WriteLine("Gemaakt door Justin Pooters");
+			Console.WriteLine("Contributors:");
+			Console.WriteLine("Aaron Knoop");
 
-                        Console.WriteLine("Starting avast download");
-                        DownloadAndExecute("https://cdn.thatgeek.dev/sah/antivirus.exe", "antivirus.exe");
+			string choice = null;
 
-                        // MalwareBytes
+			while (string.IsNullOrEmpty(choice) || !ContainsAny(choice, new[] {"j", "n"}))
+			{
+				Console.WriteLine("Typ `ja` om door te gaan of typ `nee` om te stoppen.");
+				choice = Console.ReadLine();
+				Console.Clear();
+			}
 
-                        Console.WriteLine("Starting MalewareBytes download");
-                        DownloadAndExecute("https://cdn.thatgeek.dev/sah/malwarebytes.exe", "malwarebytes.exe");
+			if (choice.ToLower().Contains("n"))
+			{
+				Console.WriteLine("Druk op Enter om af te sluiten");
+				Console.ReadLine();
+			}
+			else
+			{
+				Console.WriteLine("De installatie kan nu beginnen");
+				Console.WriteLine("Kies een van de volgende opties");
+				Console.WriteLine(
+					"1: Full install, dit installeert: ThunderBird, CCleaner, Malwarebytes, Avast anti-virus en FireFox web browser");
+				Console.WriteLine("2: AntiVirus, dit installeert Malwarebytes en Avast anti-virus");
+				Console.WriteLine("3: Standaard, Dit installeert Firefox en ThunderBird");
+				Console.WriteLine("Kis 1, 2 of 3");
 
-                        Console.WriteLine("FireFox & Thunderbird are next! :D");
-                        Console.WriteLine("Wie wil er nog internet explorer / edge gebruiken 🤮");
+				int installType = -1;
+
+				while (installType == -1)
+				{
+					if (Int32.TryParse(Console.ReadLine(), out installType))
+					{
+						if (installType > 3 || installType < 1)
+						{
+							installType = -1;
+						}
+					}
+
+					if (installType == -1)
+					{
+						Console.WriteLine("Ongeldige Input!");
+					}
+				}
+
+				Console.Clear();
+
+				bool installAvast = false;
+
+				if (installType == 1)
+				{
+					Console.WriteLine("Wil je avast ook installeren?");
+					Console.WriteLine("Kies: ja of nee");
+					string input = Console.ReadLine();
+
+					while (string.IsNullOrEmpty(input) || !ContainsAny(input, new[] {"j", "n"}))
+					{
+						Console.WriteLine("Ongeldige input!");
+						input = Console.ReadLine();
+					}
+
+					installAvast = input.ToLower().Contains("j");
+				}
+
+				if (installType < 3)
+				{
+					if (installAvast || installType == 2)
+					{
+						//avast
+
+						Console.WriteLine("Starting avast download");
+						DownloadAndExecute("https://cdn.thatgeek.dev/sah/antivirus.exe", "antivirus.exe");
+					}
+
+					// MalwareBytes
+
+					Console.WriteLine("Starting MalewareBytes download");
+					DownloadAndExecute("https://cdn.thatgeek.dev/sah/malwarebytes.exe", "malwarebytes.exe");
+				}
 
 
-                        //Firefox
+				if (installType != 2)
+				{
+					//Firefox
 
-                        Console.WriteLine("Starting firefox download");
-                        DownloadAndExecute("https://cdn.thatgeek.dev/sah/firefox.exe", "firefox.exe");
+					Console.WriteLine("Starting firefox download");
+					DownloadAndExecute("https://cdn.thatgeek.dev/sah/firefox.exe", "firefox.exe");
 
-                        //Thunderbird
+					//Thunderbird
 
-                        Console.WriteLine("Starting thunderbird download");
-                        DownloadAndExecute("https://cdn.thatgeek.dev/sah/thunderbird.exe", "thunderbird.exe");
-
-                        Console.WriteLine("Extra handy dandy tools ;P");
-
-
-                        // CCleaner
-
-                        Console.WriteLine("Starting ccleaner download");
-                        DownloadAndExecute("https://cdn.thatgeek.dev/sah/ccleaner.exe", "ccleaner.exe");
-
-                    }
-                    else
-                    {
-                        // MalwareBytes
-
-                        Console.WriteLine("Starting MalewareBytes download");
-                        DownloadAndExecute("https://cdn.thatgeek.dev/sah/malwarebytes.exe", "malwarebytes.exe");
-
-                        Console.WriteLine("FireFox & Thunderbird are next! :D");
-                        Console.WriteLine("Wie wil er nog internet explorer / edge gebruiken 🤮");
+					Console.WriteLine("Starting thunderbird download");
+					DownloadAndExecute("https://cdn.thatgeek.dev/sah/thunderbird.exe", "thunderbird.exe");
 
 
-                        //Firefox
+					if (installType == 1)
+					{
+						// CCleaner
 
-                        Console.WriteLine("Starting firefox download");
-                        DownloadAndExecute("https://cdn.thatgeek.dev/sah/firefox.exe", "firefox.exe");
+						Console.WriteLine("Starting ccleaner download");
+						DownloadAndExecute("https://cdn.thatgeek.dev/sah/ccleaner.exe", "ccleaner.exe");
+					}
+				}
+			}
+		}
 
-                        //Thunderbird
+		private static void DownloadAndExecute(string url, string outputFileName)
+		{
+#if !DEBUG
+			AutoResetEvent are = new AutoResetEvent(false);
+			var client = new WebClient();
+			client.DownloadFileCompleted += new AsyncCompletedEventHandler((sender, args) =>
+			{
+				var path = Path.Combine(Directory.GetCurrentDirectory(), outputFileName);
+				System.Diagnostics.Process.Start(path);
+				are.Set();
+			});
 
-                        Console.WriteLine("Starting thunderbird download");
-                        DownloadAndExecute("https://cdn.thatgeek.dev/sah/thunderbird.exe", "thunderbird.exe");
+			client.DownloadFileAsync(new Uri(url), outputFileName);
 
-                        Console.WriteLine("Extra handy dandy tools ;P");
+			are.WaitOne();
+			client.Dispose();
+#endif
+		}
 
+		private static bool ContainsAny(string target, string[] chars)
+		{
+			foreach (string character in chars)
+			{
+				if (target.Contains(character)) return true;
+			}
 
-                        // CCleaner
-
-                        Console.WriteLine("Starting ccleaner download");
-                        DownloadAndExecute("https://cdn.thatgeek.dev/sah/ccleaner.exe", "ccleaner.exe");
-
-                    }
-                }
-                if (gekozennummer == "2")
-                {
-                    Console.WriteLine("Cool! Antivirus, safety first ;)");
-                    
-                    // Avast    
-
-                    Console.WriteLine("Starting avast download");
-                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/antivirus.exe", "antivirus.exe");
-
-                    // MalwareBytes
-
-                    Console.WriteLine("Starting malwarebytes download");
-                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/malwarebytes.exe", "malwarebytes.exe");
-
-                    // CCleaner
-
-                    Console.WriteLine("Starting ccleaner download");
-                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/ccleaner.exe", "ccleaner.exe");
-
-                }
-
-                if (gekozennummer == "3")
-                {
-                    Console.WriteLine("Cool! internet en email zijn belangerijk hoor :P");
-
-                    //Firefox
-
-                    Console.WriteLine("Starting Browser download");
-                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/firefox.exe", "firefox.exe");
-
-                    //Thunderbird
-                    Console.WriteLine("Starting Thunderbird download");
-                    DownloadAndExecute("https://cdn.thatgeek.dev/sah/thunderbird.exe", "thunderbird.exe");
-
-                }
-            }
-        }
-        private static void DownloadAndExecute(string url, string outputFileName)
-        {
-            AutoResetEvent are = new AutoResetEvent(false);
-            var client = new WebClient();
-            client.DownloadFileCompleted += new AsyncCompletedEventHandler((sender, args) =>
-                {
-                    var path = Path.Combine(Directory.GetCurrentDirectory(), outputFileName);
-                    System.Diagnostics.Process.Start(path);
-                    are.Set();
-                }); 
-                
-            client.DownloadFileAsync(new Uri(url), outputFileName);
-
-            are.WaitOne();
-            client.Dispose();
-        }
-
-    }
+			return false;
+		}
+	}
 }
