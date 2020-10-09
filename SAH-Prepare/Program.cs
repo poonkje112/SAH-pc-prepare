2349541916e0e9346885aa1cc31886be6f4da2e1
Aaron Knoop <aaron.knoop@live.nl>
2020-10-09 13:36:04 +0200

    Removed unused usings

--- SAH-Prepare/Program.cs
+++ SAH-Prepare/Program.cs
@@ -1,15 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Net;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
 using System.Security.Principal;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SAH_Prepare
 {
